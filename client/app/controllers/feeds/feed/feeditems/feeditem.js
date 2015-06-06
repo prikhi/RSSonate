cb59f44bea23feb03e81f6350749c2acedd1a4e2
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-06-06 04:24:38 -0400

    Client: Fill in Blank Spaces & Clean Up

--- client/app/controllers/feeds/feed/feeditems/feeditem.js
+++ client/app/controllers/feeds/feed/feeditems/feeditem.js
@@ -3,18 +3,20 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   needs: ["feeds/feed"],
 
-  nextItem: Ember.computed(
-    'model', 'controllers.feeds/feed.model.items.[]', function() {
-      /* Next is Younger, Items is in descening age, so we get the previous */
-      let items = this.get('controllers.feeds/feed.model.items.[]');
+  items: Ember.computed.alias('controllers.feeds/feed.model.items.[]'),
+
+  nextItem: Ember.computed('model', 'items', function() {
+      /* Next is Younger & Items is in descening age, so we get the previous
+       * item in the array */
+      let items = this.get('items');
       let index = items.indexOf(this.get('model'));
       return items.objectAt(index - 1);
   }),
 
-  previousItem: Ember.computed(
-    'model', 'controllers.feeds/feed.model.items.[]', function() {
-      /* Previous is Older, Items is in descending age, so we get the next */
-      let items = this.get('controllers.feeds/feed.model.items.[]');
+  previousItem: Ember.computed('model', 'items', function() {
+      /* Previous is Older and Items is in descending age, so we get the next
+       * item in the array */
+      let items = this.get('items');
       let index = items.indexOf(this.get('model'));
       return items.objectAt(index + 1);
   }),
--- client/app/routes/feeds/feed.js
+++ client/app/routes/feeds/feed.js
@@ -3,10 +3,10 @@ import scrollPanelToItem from 'client/utils/scroll-panel-to-item';
 
 export default Ember.Route.extend({
   setupController: function(controller, model) {
-    this._super(controller, model);
     Ember.run.scheduleOnce('afterRender', this, function() {
       /* Scroll the Feeds nav to the selected item */
       scrollPanelToItem('#feeds-panel', 'ul li a.active');
     });
+    this._super(controller, model);
   }
 });
