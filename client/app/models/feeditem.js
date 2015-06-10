407da4c71f11ddd0cd8c608f48bbbd2da9370847
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-06-10 10:10:20 -0400

    Add Published Date to Items Table & Cleanup UI

--- client/app/models/feeditem.js
+++ client/app/models/feeditem.js
@@ -1,9 +1,14 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   feed: DS.belongsTo('feed'),
   title: DS.attr('string'),
   link: DS.attr('string'),
   description: DS.attr('string'),
-  published: DS.attr('date')
+  published: DS.attr('date'),
+
+  formattedDate: Ember.computed('published', function() {
+    return this.get('published').toLocaleDateString();
+  }),
 });
