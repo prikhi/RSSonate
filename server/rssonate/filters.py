0f912c83d7b53a072b6c095d112335b77ecce866
Pavan Rikhi <pavan.rikhi@gmail.com>
2016-11-21 21:11:48 -0500

    server: Fix Deprecated Request Property

--- server/rssonate/filters.py
+++ server/rssonate/filters.py
@@ -5,7 +5,7 @@ from rest_framework import filters
 class CoalesceFilterBackend(filters.BaseFilterBackend):
     '''Support Ember Data coalesceFindRequests.'''
     def filter_queryset(self, request, queryset, view):
-        id_list = request.QUERY_PARAMS.getlist('ids[]')
+        id_list = request.query_params.getlist('ids[]')
         if id_list:
             queryset = queryset.filter(id__in=id_list)
         return queryset
